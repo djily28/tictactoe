a8969a5fdc379e0520d56292e546b7d0708f83fa
abdou <dioufabdoukhadre8@gmail.com>
2023-09-18 15:30:50 +0200

    clean code and add functions playerName and playerTurn

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@
 // console.log())
 
 const grid = document.querySelectorAll(".grid li");
-
+const player = document.querySelector(".player");
 let count = 0;
+
+displayPlayerTurn();
 grid.forEach((li) => {
   li.addEventListener("click", function () {
     if (this.childElementCount === 0) {
-		count++;
-		const displayImg = document.createElement("img");
-		displayImg.classList.add("case-img");
-		this.appendChild(displayImg);
-		displayImg.src = count % 2 == 0 ?  "img/close.png": "img/rec.png";	
+      const displayImg = document.createElement("img");
+      displayImg.classList.add("case-img");
+      this.appendChild(displayImg);
+      displayImg.src = getPlayerTurn() == 0 ? "img/close.png" : "img/rec.png";
+      count++;
+      displayPlayerTurn();
     }
   });
 });
+function getPlayerTurn() {
+  return count % 2;
+}
+
+function displayPlayerTurn() {
+  player.innerText = `C'est au tour du joueur numéro ${
+    getPlayerTurn() + 1
+  } de jouer.`;
+}
