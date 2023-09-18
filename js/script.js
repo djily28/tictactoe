34cbb76bf7c05529571fb39866c222d48b60ff85
abdou <dioufabdoukhadre8@gmail.com>
2023-09-18 16:59:58 +0200

    result

--- js/script.js
+++ js/script.js
@@ -6,16 +6,29 @@ const grid = document.querySelectorAll(".grid li");
 const player = document.querySelector(".player");
 let count = 0;
 
+let gridStatus = [
+	['', '', ''],
+	['', '', ''],
+	['', '', ''],
+	];
+
+
 displayPlayerTurn();
-grid.forEach((li) => {
+grid.forEach((li, i) => { 
+	li.dataset.column = Math.floor(i/3 );
+	li.dataset.row = i%3;
+	console.log(i);
   li.addEventListener("click", function () {
     if (this.childElementCount === 0) {
       const displayImg = document.createElement("img");
       displayImg.classList.add("case-img");
       this.appendChild(displayImg);
       displayImg.src = getPlayerTurn() == 0 ? "img/close.png" : "img/rec.png";
-      count++;
-      displayPlayerTurn();
+    
+	  gridStatus[this.dataset.row][this.dataset.column] = getPlayerTurn();
+	  console.log(gridStatus , this.dataset.column , this.dataset.row);
+	  count++;
+	  displayPlayerTurn();
     }
   });
 });
@@ -28,3 +41,4 @@ function displayPlayerTurn() {
     getPlayerTurn() + 1
   } de jouer.`;
 }
+
