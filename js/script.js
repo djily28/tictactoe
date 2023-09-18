4e36b1c0799bf4f75ad289ef6f9987120a1f3476
abdou <dioufabdoukhadre8@gmail.com>
2023-09-18 13:37:53 +0200

    end of us2

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 
 // console.log())
 
-const grid = document.querySelectorAll('.grid li');
-const cross = document.querySelector('#cross');
+const grid = document.querySelectorAll(".grid li");
+const cross = document.querySelector("#cross");
 
-
-grid.forEach(li => {
-    li.addEventListener('click', function(){
-        const displayCross = document.createElement('img');
-        displayCross.classList.add('case-img'); 
-        displayCross.src = 'img/close.png'; 
-        li.appendChild(displayCross);
-        });
-    });
+grid.forEach((li) => {
+  li.addEventListener("click", function () {
+    if (this.childElementCount === 0) {
+      const displayCross = document.createElement("img");
+      displayCross.classList.add("case-img");
+      displayCross.src = "img/close.png";
+      this.appendChild(displayCross);
+    }
+  });
+});
