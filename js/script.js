f0d2003a74778cefc81762835aa0b1cc2ea87fcd
abdou <dioufabdoukhadre8@gmail.com>
2023-09-18 12:06:21 +0200

    clean class css

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const cross = document.querySelector('#cross');
 grid.forEach(li => {
     li.addEventListener('click', function(){
         const displayCross = document.createElement('img');
+        displayCross.classList.add('case-img'); 
         displayCross.src = 'img/close.png'; 
         li.appendChild(displayCross);
         });
