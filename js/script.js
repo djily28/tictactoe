e0945fa292526451e6e61549937550b113de58f1
abdou <dioufabdoukhadre8@gmail.com>
2023-09-18 11:11:33 +0200

    add method foreach to grid and create const cross

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('coucou');
+// console.log('coucou');
+
+// console.log())
+
+const grid = document.querySelectorAll('.grid li');
+const cross = document.querySelector('#cross');
+
+grid.forEach(li => {
+    li.addEventListener('click', function(){
+        });
+    });
